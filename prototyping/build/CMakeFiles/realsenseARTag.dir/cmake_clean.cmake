79226fcc40509996410bd76409082e71582ec947
Liam Thurston <98102534@student.uts.edu.au>
2020-07-18 10:58:37 +1000

    Initial Prototyping

---
+++ prototyping/build/CMakeFiles/realsenseARTag.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/realsenseARTag.dir/src/main.cpp.o"
+  "CMakeFiles/realsenseARTag.dir/src/buffer.cpp.o"
+  "devel/lib/realsenseARTag/realsenseARTag.pdb"
+  "devel/lib/realsenseARTag/realsenseARTag"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/realsenseARTag.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
