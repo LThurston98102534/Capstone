79d9bf0c5c990af19b472c0ea89ab9c312a54e75
Liam Thurston <98102534@student.uts.edu.au>
2020-09-04 07:10:35 +0200

    Cleaning out repo

--- prototyping/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user/git2/Capstone/prototyping
-# Build directory: /home/user/git2/Capstone/prototyping/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
--- prototyping/build/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/user/git2/Capstone/prototyping/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
